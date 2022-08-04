04014db1d7cba39fea06c6ce7c82415d5b1127a7
Sneha-K-Sajith <snehaksajith@techfriar.com>
2022-08-04 10:56:10 +0530

    commit of figma_project

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let menu_btn = document.getElementById("menu_btn");
 let menu = document.getElementById("menu");
-menu_btn.addEventListener("click", function handleClick(){
-    menu.classList.toggle("active");
-  });
+let search = document.getElementById("search");
+let search_box = document.getElementById("search_box");
+let search_switch = document.getElementById("search_switch");
+menu_btn.addEventListener("click", function handleClick() {
+  menu.classList.toggle("active");
+});
+search_switch.addEventListener("click", function handleClick() {
+  search.classList.toggle("active");
+  search_box.classList.toggle("active");
+});
